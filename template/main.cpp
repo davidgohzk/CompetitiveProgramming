048109d15f45dde8fdd27aa6b59ab61893f8866b
David Goh <david.gohzhekai@gmail.com>
2022-10-11 15:42:09 +0800

    added tempalte

--- template/main.cpp
+++ template/main.cpp
@@ -2,11 +2,17 @@
 
 using namespace std;
 
-void solve() {
 
+void solve() {
+    cout << "hello world";
 }
 
 int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     int tc = 1;
@@ -14,5 +20,6 @@ int main() {
     for (int t = 1; t < tc; t++){
         // cout << "Case #" << t << ": ";
         solve();
+        cout << "hello world";
     }
 }
